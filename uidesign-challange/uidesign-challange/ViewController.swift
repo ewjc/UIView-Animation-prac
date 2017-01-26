d46714e0fed0ddb9630c59c6ecc8e34ff9b85db8
ewjc <ericswong7@gmail.com>
2017-01-26 12:10:29 -0800

    chess board function with board view

--- uidesign-challange/uidesign-challange/ViewController.swift
+++ uidesign-challange/uidesign-challange/ViewController.swift
@@ -8,17 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let frameWidth = view.bounds.width - 10
-        let frameHeight = view.bounds.height - 10
-        let frame = CGRect(x: 0, y: 0, width: frameWidth, height: frameHeight)
-        let redView = UIView(frame: frame.insetBy(dx: 10, dy: 10))
-        redView.backgroundColor = UIColor.red
-        view.addSubview(redView)
-        
         
     }
 
@@ -26,7 +19,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Challange 1:
 
+    func redSquare() {
+        let frameWidth = view.bounds.width - 10
+        let frameHeight = view.bounds.height - 10
+        let frame = CGRect(x: 0, y: 0, width: frameWidth, height: frameHeight)
+        let redView = UIView(frame: frame.insetBy(dx: 10, dy: 10))
+        redView.backgroundColor = UIColor.red
+        view.addSubview(redView)
+    }
+    
+    // MARK: Challange 2:
+    var width: CGFloat?
+    var height: CGFloat?
+    var boardColumns: Int?
+    var boardRows: Int?
+    
+    func chessBoard(width: CGFloat, height: CGFloat, boardColumn: Int, boardRow: Int) {
+        let boardFrame = CGRect(x: 0, y: 0, width: width, height: height)
+        let boardView = UIView(frame: boardFrame)
+        boardView.layer.borderColor = UIColor.black.cgColor
+        boardView.layer.borderWidth = 2
+        
+        
+        
+        
+        
+    
+    }
 
 }
 
