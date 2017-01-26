eb2e2e32aead3732e813628804a837cb38650802
ewjc <ericswong7@gmail.com>
2017-01-26 11:46:59 -0800

    chllg 1 complete

--- uidesign-challange/uidesign-challange/ViewController.swift
+++ uidesign-challange/uidesign-challange/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let frameWidth = view.bounds.width - 10
+        let frameHeight = view.bounds.height - 10
+        let frame = CGRect(x: 0, y: 0, width: frameWidth, height: frameHeight)
+        let redView = UIView(frame: frame.insetBy(dx: 10, dy: 10))
+        redView.backgroundColor = UIColor.red
+        view.addSubview(redView)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
